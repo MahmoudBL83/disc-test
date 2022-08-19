12954cf20b85b69e7efc8b3cb2606b605d3f431a
MahmoudBL83 <95527734+MahmoudBL83@users.noreply.github.com>
2022-08-19 18:36:48 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -67,7 +67,7 @@ navs[6].addEventListener('click',()=>{
     location.replace('https://www.iptc.com.sa/en/contacts/')
 })
 navs[7].addEventListener('click',()=>{
-    location.replace('../registirationEN.html')
+    location.replace('./registirationEN.html')
 })
 
 /**************************************************************/
@@ -125,7 +125,7 @@ asides[3].addEventListener('click',()=>{
     asides[3].querySelector('.slide').classList.add('hidden')
 })
 asides[3].querySelector('span').addEventListener('click',()=>{
-    location.replace('../registiration.html')
+    location.replace('./registiration.html')
 })
 asides[4].addEventListener('click',()=>{
     location.replace('https://www.iptc.com.sa/en/clients/')
@@ -137,7 +137,7 @@ asides[6].addEventListener('click',()=>{
     location.replace('https://www.iptc.com.sa/en/contacts/')
 })
 asides[7].addEventListener('click',()=>{
-    location.replace('../registirationEN.html')
+    location.replace('./registirationEN.html')
 })
 
 document.querySelector('.fa-bars').addEventListener('click',(e)=>{
